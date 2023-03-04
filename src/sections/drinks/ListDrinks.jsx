d777cd8cbf380afd199c7a6108093f7ce1b0baf4
JacquelineFM <jfloresm.dev@gmail.com>
2023-03-03 19:55:36 -0600

    DRIN-10 feat: add pagination
    - Add a PaginationList component to enable navigation
    between the list of drinks obtained from the user's search in the
    ListDrinks component.
    - Add a custom hook called usePagination that provides
    functions and variables for currentPage, currentItems,
    goToNextPage, goToPrevPage, goToPage, goToFirstPage,
    goToLastPage, and totalPages for the Pagination component.

--- src/sections/drinks/ListDrinks.jsx
+++ src/sections/drinks/ListDrinks.jsx
@@ -1,7 +1,10 @@
 // react-bootstrap
 import { Row } from "react-bootstrap";
+// components
+import PaginationList from "../../components/PaginationList";
 // hooks
 import useDrinks from "../../hooks/useDrinks";
+import usePagination from "../../hooks/usePagination";
 //
 import Drink from "./Drink";
 
@@ -9,12 +12,33 @@ import Drink from "./Drink";
 
 const ListDrinks = () => {
   const { drinks } = useDrinks();
+  const {
+    currentPage,
+    currentItems,
+    goToNextPage,
+    goToPrevPage,
+    goToPage,
+    goToFirstPage,
+    goToLastPage,
+    totalPages,
+  } = usePagination(drinks, 16);
 
   return (
     <Row className="g-4m">
-      {drinks.map((drink) => (
+      {currentItems.map((drink) => (
         <Drink key={drink.idDrink} drink={drink} />
       ))}
+      {Boolean(currentItems.length) && (
+        <PaginationList
+          pages={totalPages}
+          currentPage={currentPage}
+          setCurrentPage={goToPage}
+          goToNextPage={goToNextPage}
+          goToPrevPage={goToPrevPage}
+          goToFirstPage={goToFirstPage}
+          goToLastPage={goToLastPage}
+        />
+      )}
     </Row>
   );
 };
